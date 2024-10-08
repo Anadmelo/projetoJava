ea814b7c8fb99ace0dcbd528b44129c685581128
Ana Julia <estudosanajulia190@gmail.com>
2024-10-08 07:25:17 -0300

    salve

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('foto')
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
     var data = new Date()
     var hora = 20
     //var hora = data.getHours()
     msg.innerHTML = 'Agora são '+ hora + ' horas.'
     if (hora >= 0 && hora < 12){
             //bom dia
-        img.src="img/manha.jpg"
+        img.setAttribute('src', 'manha.jpg')
         document.body.style.background = '#E0D399'
     } else if(hora >= 12 && hora < 18 ){
         //boa tarde
-        img1.src="img/tarde.jpg"
+        img.setAttribute('src', 'tarde.jpg')
     } else {
         //boa noite
-        img2.src="img/noite.jpg"
+        img.setAttribute('src', 'noite.jpg')
         document.body.style.background = 'gray'
     }
 }
